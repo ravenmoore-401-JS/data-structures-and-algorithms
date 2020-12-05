2325e8ac44d38938e8fbb4b6904f652d48805961
Matt Ravenmoore <m.ravenmoore@gmail.com>
2020-12-04 18:59:36 -0800

    401 prework update

--- code-challenges/11.reg-exp-round2/challenges-11.test.js
+++ code-challenges/11.reg-exp-round2/challenges-11.test.js
@@ -48,7 +48,7 @@ Note: if you ever need to validate an email using a regex in practice, the Inter
 ------------------------------------------------------------------------------------------------ */
 
 const validateEmail = (email) => {
-  return /\S+.@\S+.\.\S+/.test(email);
+  return /\S+@\S+.\.\S+/.test(email);
 };
 
 /* ------------------------------------------------------------------------------------------------
