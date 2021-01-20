4c4f26a9707453e7327fbc392f27e1f56e400388
Matt Ravenmoore <m.ravenmoore@gmail.com>
2021-01-19 17:30:39 -0800

    wrote code to resub need to test tho

--- javascript/code-challenges/repeated-words/wordTable.js
+++ javascript/code-challenges/repeated-words/wordTable.js
@@ -10,19 +10,64 @@ class HashMap {
 
   hash(word){
     const hashNum = 42;
-      //has later
+    const letterCount = 0;
+
+    for(let i=0;i <word.length;i++){
+      letterCount + word.charCodeAt(i);
+      letterCount* hashNum%this.size;
+    }
+    console.log('hashed value', letterCount);
+    return letterCount;
   }
 
   set(word){
     const hashedKey = this.hash(word);
-
     const thingToAdd = {[word]:word};
 
     // handle overlap
-    if (!this.map[hashedKey]){this.map[hashedKey] = new LL();}
-    // add hased key:value pair to tabele at the hash index
-    this.map[hashedKey].add(thingToAdd);
-  }
+    if (!this.map[hashedKey]){
+      this.map[hashedKey] = new LL();
+      this.map[hashedKey].add(thingToAdd);
+      return;
+    }
+    if(this.map[hashedKey]){
+      let current = this.map[hashedKey].head;
+      while(current){
+        if(current.value === current.next.value)return current.value;
+        current = current.next;
+      }
+      return null;
 
+    }
+  }
 }
+
+const stringMap = new HashMap(500);
+
+function repeatedWord(string){
+
+  const splitString = (str) =>{
+    let wordsArr = [];
+    let tempStr = '';
+    for(let i=0;i< str.length;i++){
+      if(str[i] !== ' '){
+        tempStr += str[i];
+      }else{
+        wordsArr = [...wordsArr,tempStr];
+        tempStr = '';
+      }
+      return wordsArr;
+    }
+  };
+
+  const words = splitString(string);
+  for(let i=0;i<words.length;i++){
+    stringMap.set(words[i]);
+  }
+}
+
+const testString= "I am a Purple Hippo, and there is a Purple rino over there!"
+const test = repeatedWord(testString);
+
+console.log(test);
 module.exports = HashMap;
